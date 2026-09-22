eab2b3d4737432ca51b684a21c7fa8114b56b7e5
niraj551 <nirajbhusal2077@gmail.com>
2026-09-22 21:58:52 +0545

    api updates

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 // services/api.js
 import axios from "axios";
 
-const API_BASE_URL = "https://secureshield-backend-1.onrender.com";
+const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ||
+  "https://secureshield-backend-1.onrender.com";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
